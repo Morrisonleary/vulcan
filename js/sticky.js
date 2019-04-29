a75c4f985cde4fed7161da69e8359c3e001b50ae
Morrisonleary <Morrisonleary@gmail.com>
2019-04-29 09:04:41 -0600

    Add files via upload
    Updated button colors and added box shadow.

--- js/sticky.js
+++ js/sticky.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
-           $(".menu-icon").on("click", function() {
-                 $("nav ul").toggleClass("showing");
-           });
-     });
+  $(".menu-icon").on("click", function() {
+    $("nav ul").toggleClass("showing");
+  });
+});
 
-     // Scrolling Effect
+// Scrolling Effect
 
-     $(window).on("scroll", function() {
-           if($(window).scrollTop()) {
-                 $('.fixed-top').addClass('black');
-           }
-
-           else {
-                 $('.fixed-top').removeClass('black');
-           }
-     });
+$(window).on("scroll", function() {
+  if ($(window).scrollTop()) {
+    $('.fixed-top').addClass('black');
+  } else {
+    $('.fixed-top').removeClass('black');
+  }
+});
